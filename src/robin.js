736891d9872b21d0da6270831043d1f216539a87
Gavin Mogan <gavin@gavinmogan.com>
2016-12-05 21:43:39 -0800

    Add support for @hubot rooms Vancouver
    * Fuzzy match like the existing integration does
    * Clean up eslint based fixes now that it actually runs
    * clean up some dependancies

--- src/robin.js
+++ src/robin.js
@@ -18,49 +18,63 @@
 
 const once = require('lodash.once');
 const Robin = require('robin-js-sdk');
+const fuzzy = require('fuzzy');
 
 const robinToken = process.env.ROBIN_TOKEN;
 const robinOrg = process.env.ROBIN_ORGANIZATION;
 
 module.exports = function(robot) {
   let robin = new Robin(robinToken);
-  let location = null;
 
   const getLocations = once(function getLocations() {
     return robin.api.organizations.locations.get(robinOrg).then(function(response) {
       return response.getData();
     });
   });
-  robot.respond(/rooms/, function(res) {
+
+  function filter(locations, filter) {
+    if (!filter) { return locations; }
+    const options = {
+      extract: l => l.name
+    };
+    return fuzzy.filter(filter, locations, options).map(i => i.original).slice(0);
+  }
+
+  function handler(res) {
     robin = new Robin(robinToken);
-    return getLocations().then(locations => {
-      return Promise.all(locations.map(l => robin.api.locations.spaces.get(l.id)))
-        .then(locations => {
-          let final_response = "";
-          locations.forEach(location => {
-            for (let space of location.getData()) {
-              let { name } = space;
-              if (!space.current_event) {
-                let time_notice = "";
-                if (space.next_event) {
-                  var next_event_date = new Date(space.next_event.started_at);
+    return getLocations()
+      .then(locations => filter(locations, res.match[1]))
+      .then(locations => {
+        return Promise.all(locations.map(l => robin.api.locations.spaces.get(l.id)))
+          .then(locations => {
+            let final_response = '';
+            locations.forEach(location => {
+              for (let space of location.getData()) {
+                let { name } = space;
+                if (!space.current_event) {
+                  let time_notice = '';
+                  if (space.next_event) {
+                    var next_event_date = new Date(space.next_event.started_at);
+                  }
+                  let time_diff = next_event_date ? (next_event_date.getTime() - Date.now()) / 1000 : 0;
+                  if (time_diff && time_diff < (60 * 60)) { // 1 hour
+                    time_notice = ` for ${Math.round(time_diff / 60)} minutes`;
+                  }
+                  final_response += `\n${name} is free${time_notice}`;
                 }
-                let time_diff = next_event_date ? (next_event_date.getTime() - Date.now()) / 1000 : 0;
-                if (time_diff && time_diff < (60 * 60)) { // 1 hour
-                  time_notice = ` for ${Math.round(time_diff / 60)} minutes`;
-                }
-                final_response += `\n${name} is free${time_notice}`;
               }
-            }
+            });
+            return final_response;
           });
-          return final_response;
+      }).then(final_response => {
+        return res.reply(final_response ? final_response : 'no rooms available :(');
+      }).catch(function(err) {
+        console.error('robin err', err); // eslint-disable-line no-console
+        throw err;
       });
-    }).then(final_response => {
-      return res.reply(final_response ? final_response : "no rooms available :(");
-    }).catch(function(err) {
-      console.error('robin err', err)
-      throw err;
-    });
-  });
+  }
+
+  robot.respond(/rooms$/, handler);
+  robot.respond(/rooms (.*)$/, handler);
 };
 
--- test/robin-test.js
+++ test/robin-test.js
@@ -1,61 +1,60 @@
+/* eslint-env mocha */
 process.env.EXPRESS_PORT = process.env.PORT = 0;
 process.env.ROBIN_TOKEN = 'FAKE FAKE TOKEN';
 process.env.ROBIN_ORGANIZATION = 'fake_org';
 let chai = require('chai');
-let sinon = require('sinon');
 let nock = require('nock');
-chai.use(require('sinon-chai'));
 let BotHelper = require('hubot-test-helper');
 
 let { expect } = chai;
 
 function setupNock() {
-  nock('https://api.robinpowered.com:443', {"encodedQueryParams":true})
+  nock('https://api.robinpowered.com:443', {'encodedQueryParams':true})
     .get('/v1.0/organizations/fake_org/locations')
     .reply(200, {
-      "meta":{
-        "status_code":200,"status":"OK","message":"","more_info":{}
+      'meta':{
+        'status_code':200,'status':'OK','message':'','more_info':{}
       },
-      "data":[
-        {"id":2117,"account_id":2672,"name":"UK","description":"","address":"1 London Bridge Street, London, United Kingdom","image":"","latitude":51.5051679,"longitude":-0.0873985,"updated_at":"2016-03-04T21:00:57+0000","created_at":"2016-03-04T21:00:57+0000"},
-        {"id":2033,"account_id":2672,"name":"Vancouver","description":"","address":"","image":"","latitude":0,"longitude":0,"updated_at":"2016-02-24T21:42:25+0000","created_at":"2016-02-24T21:42:25+0000"},
-        {"id":576,"account_id":2672,"name":"HQ","description":"","address":"538 Bryant St, San Francisco, 94107, California, United States","image":"","latitude":37.7807998,"longitude":-122.3969095,"updated_at":"2015-07-20T18:24:07+0000","created_at":"2015-05-26T19:59:38+0000"}
+      'data':[
+        {'id':2117,'account_id':2672,'name':'UK','description':'','address':'1 London Bridge Street, London, United Kingdom','image':'','latitude':51.5051679,'longitude':-0.0873985,'updated_at':'2016-03-04T21:00:57+0000','created_at':'2016-03-04T21:00:57+0000'},
+        {'id':2033,'account_id':2672,'name':'Vancouver','description':'','address':'','image':'','latitude':0,'longitude':0,'updated_at':'2016-02-24T21:42:25+0000','created_at':'2016-02-24T21:42:25+0000'},
+        {'id':576,'account_id':2672,'name':'HQ','description':'','address':'538 Bryant St, San Francisco, 94107, California, United States','image':'','latitude':37.7807998,'longitude':-122.3969095,'updated_at':'2015-07-20T18:24:07+0000','created_at':'2015-05-26T19:59:38+0000'}
       ],
-      "paging":{"page":1,"per_page":10}
-    })
-  nock('https://api.robinpowered.com:443', {"encodedQueryParams":true})
+      'paging':{'page':1,'per_page':10}
+    });
+  nock('https://api.robinpowered.com:443', {'encodedQueryParams':true})
     .get(/\/v1.0\/locations\/(\d+)\/spaces/)
     .times(3)
     .reply(function(uri) {
-      const roomId = this.req.path.match(/\/v1.0\/locations\/(\d+)\/spaces/)[1];
+      const roomId = uri.match(/\/v1.0\/locations\/(\d+)\/spaces/)[1];
       return [
         200,
         {
-          "meta":{"status_code":200,"status":"OK","message":"","more_info":{}},
-          "data":[
+          'meta':{'status_code':200,'status':'OK','message':'','more_info':{}},
+          'data':[
             {
-              "id":roomId*3000,
-              "location_id":2117,
-              "name":"Room " + roomId,
-              "description":"",
-              "image":"https://static.robinpowered.com/reimagine/images/ab5c501eb41e92b579eaad253df17b045800e756.png",
-              "discovery_radius":3.5,
-              "capacity":null,
-              "type":null,
-              "is_accessible":false,
-              "is_disabled":false,
-              "is_dibsed":false,
-              "updated_at":"2016-03-04T21:02:17+0000",
-              "created_at":"2016-03-04T21:02:17+0000",
-              "current_event":null,
-              "next_event":null
+              'id':roomId*3000,
+              'location_id':2117,
+              'name':'Room ' + roomId,
+              'description':'',
+              'image':'https://static.robinpowered.com/reimagine/images/ab5c501eb41e92b579eaad253df17b045800e756.png',
+              'discovery_radius':3.5,
+              'capacity':null,
+              'type':null,
+              'is_accessible':false,
+              'is_disabled':false,
+              'is_dibsed':false,
+              'updated_at':'2016-03-04T21:02:17+0000',
+              'created_at':'2016-03-04T21:02:17+0000',
+              'current_event':null,
+              'next_event':null
             }
           ],
-          "paging":{"page":1,"per_page":10}
+          'paging':{'page':1,'per_page':10}
         }
-      ]
+      ];
     });
-};
+}
 
 let scriptHelper = new BotHelper('../src/robin.js');
 describe('robin', function() {
@@ -70,15 +69,15 @@ describe('robin', function() {
     nock.cleanAll();
   });
 
-  describe("help", () => {
-    it("lists help", () => {
+  describe('help', () => {
+    it('lists help', () => {
       expect(room.robot.helpCommands()).to.eql([
         'hubot rooms - get all free rooms in the office'
       ]);
     });
   });
 
-  describe('responds to help and includes rooms command', function() {
+  describe('responds rooms command', function() {
     beforeEach(function(done) {
       room.user.say('santi', '@hubot rooms');
       setTimeout(done, 100);
@@ -88,6 +87,32 @@ describe('robin', function() {
       expect(room.messages).to.eql([
         ['santi', '@hubot rooms'],
         ['hubot', '@santi \nRoom 2117 is free\nRoom 2033 is free\nRoom 576 is free']
+      ]);
+    });
+  });
+  describe('responds rooms command', function() {
+    beforeEach(function(done) {
+      room.user.say('santi', '@hubot rooms');
+      setTimeout(done, 100);
+    });
+
+    it('rooms across all locations is returned', function() {
+      expect(room.messages).to.eql([
+        ['santi', '@hubot rooms'],
+        ['hubot', '@santi \nRoom 2117 is free\nRoom 2033 is free\nRoom 576 is free']
+      ]);
+    });
+  });
+  describe('responds to single location', function() {
+    beforeEach(function(done) {
+      room.user.say('santi', '@hubot rooms couver');
+      setTimeout(done, 100);
+    });
+
+    it('single room is responded', function() {
+      expect(room.messages).to.eql([
+        ['santi', '@hubot rooms couver'],
+        ['hubot', '@santi \nRoom 2033 is free']
       ]);
     });
   });
